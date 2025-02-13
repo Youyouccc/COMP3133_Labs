c1cc9bd43bab447d39c1b92b4505566b0591541c
Cheng Shi <cheng.shi@georgebrown.ca>
2025-02-12 21:24:40 -0500

    graphQL schema

---
+++ w6_3133_nodejs_express_apollo_graphql/schema.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Movie {
+        id: ID!
+        title: String!
+        director: String!
+        year: Int!
+    }
+    
+    type Query {
+        movies: [Movie]
+        movie(id: ID!): Movie
+    }
+
+    type Mutation {
+        addMovie(title: String!, director: String!, year: Int!): Movie
+        updateMovie(id: ID!, title: String!, director: String!, year: Int): Movie
+        deleteMovie(id: ID!): Movie
+    }
+
+`;
+module.exports = typeDefs;
